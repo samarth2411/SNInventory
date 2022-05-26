4801e8f9781e0e6e45d83269a4a77f90e9d101ca
Nitya <nityaagarwal0218@gmail.com>
2022-05-27 01:56:22 +0530

    create a main window controller in controller package  and its fxml file

--- src/main/java/io/dbc/github/sninventory/SNApplication.java
+++ src/main/java/io/dbc/github/sninventory/SNApplication.java
@@ -21,7 +21,7 @@ public class SNApplication extends Application {
                 Screen.getPrimary().getBounds().getMinX(),
                 Screen.getPrimary().getBounds().getMinY()
         );
-        stage.setTitle("Hello!");
+        stage.setTitle("SNInventory");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/io/dbc/github/sninventory/controller/LoginController.java
+++ src/main/java/io/dbc/github/sninventory/controller/LoginController.java
@@ -1,11 +1,15 @@
 package io.dbc.github.sninventory.controller;
 
+import io.dbc.github.sninventory.SNApplication;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -23,16 +27,16 @@ public class LoginController {
     // a method to control the button
     @FXML
     public void onLoginButtonClick(ActionEvent actionEvent) throws IOException {
-
-        String username = usernameTextField.getText();
-        String password = passwordField.getText();
-
-        if (username.isBlank() || password.isBlank()) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("empty fields");
-            alert.setContentText("Please make sure that the username and password fields are not empty");
-            alert.showAndWait();
-        }
+//
+//        String username = usernameTextField.getText();
+//        String password = passwordField.getText();
+//
+//        if (username.isBlank() || password.isBlank()) {
+//            Alert alert = new Alert(Alert.AlertType.ERROR);
+//            alert.setTitle("empty fields");
+//            alert.setContentText("Please make sure that the username and password fields are not empty");
+//            alert.showAndWait();
+//        }
 
         // JDBC
         // create connection to database
@@ -41,20 +45,20 @@ public class LoginController {
         // correct, then open main windows, and call stage.close();
         // open the main window
 
-        if ("samarth".equals(username) && "password123".equals(password)) {
+//        if ("samarth".equals(username) && "password123".equals(password)) {
+//
+//        }
 
-        }
-
-//        Stage stage = new Stage();
-//        FXMLLoader fxmlLoader =
-//                new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-//        Scene scene = new Scene(
-//                fxmlLoader.load(),
-//                Screen.getPrimary().getBounds().getMinX(),
-//                Screen.getPrimary().getBounds().getMinY()
-//        );
-//        stage.setTitle("Hello!");
-//        stage.setScene(scene);
-//        stage.show();
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader =
+                new FXMLLoader(SNApplication.class.getResource("mainWindow-view.fxml"));
+        Scene scene = new Scene(
+                fxmlLoader.load(),
+                Screen.getPrimary().getBounds().getMinX(),
+                Screen.getPrimary().getBounds().getMinY()
+        );
+        stage.setTitle("SNInventory");
+        stage.setScene(scene);
+        stage.show();
     }
 }
